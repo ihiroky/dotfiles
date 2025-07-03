d9ba2943fc9896f2c816f40037dcf352beff5b14
Hiroki Itoh <ihiroky@gmail.com>
2025-07-03 21:55:52 +0900

    chore: Change leader key

--- private_dot_config/private_wezterm/private_wezterm.lua
+++ private_dot_config/private_wezterm/private_wezterm.lua
@@ -113,7 +113,7 @@ config.key_tables = {
   }
 }
 
-config.leader = { key = ' ', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.leader = { key = 'q', mods = 'CTRL', timeout_milliseconds = 1000 }
 config.keys = {
   -- mux control
   {
